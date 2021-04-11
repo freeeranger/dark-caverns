4ecdc383ec42596ad6b85dd9d13c9475b5089b96
Freeranger <freeranger05@gmail.com>
2021-04-11 13:10:17 +0200

    Added luminite helmet (w/o special functionality)

--- src/main/java/com/freeranger/dark_caverns/registry/CustomItems.java
+++ src/main/java/com/freeranger/dark_caverns/registry/CustomItems.java
@@ -1,6 +1,9 @@
 package com.freeranger.dark_caverns.registry;
 
 import com.freeranger.dark_caverns.DarkCaverns;
+import com.freeranger.dark_caverns.armor.LuminiteArmorMaterial;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,5 +15,14 @@ public class CustomItems {
     public static final RegistryObject<Item> LUMINITE_DUST = ITEMS.register(
             "luminite_dust",
             () -> new Item(new Item.Properties().tab(CustomItemGroups.GROUP))
+    );
+
+    public static final RegistryObject<Item> LUMINITE_HELMET = ITEMS.register(
+            "luminite_helmet",
+            () -> new ArmorItem(
+                    LuminiteArmorMaterial.LUMINITE,
+                    EquipmentSlotType.HEAD,
+                    new Item.Properties().tab(CustomItemGroups.GROUP)
+            )
     );
 }
